3ec5e2ceb013d75bf99515750338816341441f98
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-10 22:12:48 +0500

    beginner

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// // 1-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // 2-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// console.log(sum);
+// // 3-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 4-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 == 1) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 5-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// // 6-MASALA JAVOBI
+// let arr = [232, 12, 3, 8, -1, -9];
+// let max = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+// // 7-MASALA JAVOBI
+// let arr = [232, 12, 3, 8, -1, -9];
+// let min = arr[0];
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
+// // 8-MASALA JAVOBI
+// let arr = [23, 12, 3, 8, -1, -9];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+// let res = sum / arr.length;
+// console.log(Math.trunc(res));
+
+// // 9-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, -5, 6, 7, -80, 9, 10];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     res.push(arr[i]);
+//   }
+// }
+// console.log(res);
+
+// // 10-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, -5, 6, 7, -80, 9, 10];
+// let res = 1;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 0) {
+//     res = arr[i] ** 2;
+//   }
+//   console.log(res);
+// }
+
+// // 11-MASALA JAVOBI
+// let arr = [1, 2, 3, 4, 5];
+// let res = [];
+// for (let i = 0; i < arr.length; i++) {
+//   res.push(arr[i], arr[i]);
+// }
+// console.log(res);
+
+// // 12-MASALA JAVOBI
+// Buni yaxshi tushunmadim va ishlay olmadim
+
+// // 13-MASALA JAVOBI
